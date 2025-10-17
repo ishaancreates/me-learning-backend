388d1583a62cfa192ea9dff823af44b9f9e15b8e
Ishaan Pandey <ishaanmaybe@gmail.com>
2025-10-17 14:47:11 +0530

    linked css

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ app.use(morgan('dev'))
 //built in middleware
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
+app.use(express.static("public"))
 
 app.set ("view engine", 'ejs')
 
