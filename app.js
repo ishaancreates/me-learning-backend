0d38e65db08f9f0897b479fa397696dc3d38196a
Ishaan Pandey <ishaanmaybe@gmail.com>
2025-10-17 16:39:32 +0530

    user creaed in db

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require('express');
-const morgan = require('morgan')
-
-const app = express()
+const morgan = require('morgan');
+const app = express();
+const userModel = require('./models/user')
+const dbConnection = require('./config/db')
 
 app.use(morgan('dev'))
 
@@ -22,11 +23,28 @@ app.get('/about',(req,res)=>{
     res.render("This is about page")
 })
 
+app.get('/register',(req,res) => {
+    res.render('register')
+})
+
+app.post('/register',async (req,res) => {
+
+    const {username, email,password} = req.body
+
+    const newUser = await userModel.create({
+        username: username,
+        email: email,
+        password: password
+    })
+
+    res.send(newUser)
+})
+
 app.post('/get-form-data',(req,res) => {
     console.log(req.body)
     res.send('data received')
 })
 
-app.listen(3000,(e)=>{
+app.listen(3000,(e)=>{ 
     console.log("Server is running")
 })
